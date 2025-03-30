8f14db7c92c77fb8518e467f3e8c1c2509c1d2d5
Malimar555132 <jamesoconnor132@gmail.com>
2025-03-30 03:29:12 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+// black is wall
+// green is start postiion
+// light blue is where been
+// dark blue is fronteer
+// white is not been
+// purple is your character
+// yellow is the end
+// 
+// to do
+// make it so character can only move in tile positions
+// 
+// make it so uses algorithym to decide where to go
+// 
+// have atleast 3 algorithyms
+// DFS
+// BFS
+// random movemnets
+// 
+// eventually use the neuronet AI to try and solve the maze the fastest maybe do in anotehr program
+tiles.setCurrentTilemap(tilemap`level1`)
